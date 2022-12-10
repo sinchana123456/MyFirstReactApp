140a5cf88c9e56bfe1a571c1f028bf1d7ef7cec5
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2022-12-10 15:18:45 +0530

    JSX limitation and solution and Wrappering

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import AddUsers from './components/Users/AddUsers';
 import UsersList from './components/Users/UserList';
 
@@ -16,10 +16,10 @@ function App() {
       });
     };
   return (
-    <div>
+    <Fragment>
       <AddUsers onAddUser={addUserHandler}/>
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
