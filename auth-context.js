457777b9a9b354c468329d7fddc431aa4e5e43c5
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2022-12-13 19:04:20 +0530

    ForwardRef

--- auth-context.js
+++ auth-context.js
@@ -2,36 +2,38 @@ import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
-  onLogout: ()=>{},
-  onLogin:(email, password)=>{}
+  onLogout: () => {},
+  onLogin: (email, password) => {}
 });
 
 export const AuthContextProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const storedLoginInfo = localStorage.getItem('isLoggedIn');
-      if(storedLoginInfo === 'yes'){
-        setIsLoggedIn(true);
-      }
-    }, []);
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
-  const loginHandler = (email, password) => {
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const logoutHandler = () => {
     localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
   return (
     <AuthContext.Provider
       value={{
         isLoggedIn: isLoggedIn,
         onLogout: logoutHandler,
-        onLogin: loginHandler
-      }} 
+        onLogin: loginHandler,
+      }}
     >
       {props.children}
     </AuthContext.Provider>
